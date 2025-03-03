806fe272562b9151d9769823153d65a893292cde
Jonas Engström <j.e.engstrom@gmail.com>
2025-03-03 13:07:34 +0100

    Finshed question 2 under the Feldkamp section.

--- data-raw/feldkamp.R
+++ data-raw/feldkamp.R
@@ -2,6 +2,19 @@
 
 feldkamp <-
   file.path('data-raw/Feldkamp.xlsx') |>
-  readxl::read_xlsx()
+  readxl::read_xlsx() |>
+  mutate(
+    across(
+      c(
+        case,
+        m_inf
+      ),
+      ~ case_match(
+        .,
+        0 ~ FALSE,
+        1 ~ TRUE
+      )
+    )
+  )
 
 usethis::use_data(feldkamp, overwrite = TRUE)
