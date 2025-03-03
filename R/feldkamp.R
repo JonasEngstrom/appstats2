e71bae55d893813cbe113cc9f430be9113a50305
Jonas Engström <j.e.engstrom@gmail.com>
2025-03-03 14:28:18 +0100

    Finished exercise 1. Fixes #10.

--- R/feldkamp.R
+++ R/feldkamp.R
@@ -6,8 +6,8 @@
 #' @format ## `feldkamp` A data frame with 5,429 rows and 3 columns:
 #' \describe{
 #'   \item{id}{Participant ID.}
-#'   \item{case}{Unclear variable.}
-#'   \item{m_inf}{Unclear variable.}
+#'   \item{case}{Case of gastroschisis in newborn.}
+#'   \item{m_inf}{Genitourinary infection in the month before conception to the end of the first trimester, in the mother.}
 #' }
 #' @md
 #' @source
